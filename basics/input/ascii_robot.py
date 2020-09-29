9af070e9420b9a60e4748dedb21d229860b8c1f9
repl.it user <replituser@example.com>
2020-09-29 09:59:40 +0000

    added code to demonstrate the use of different data types

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,7 @@
+print("Give me an eye shape:")
+eye = input()
+print("beeps expression is:")
+print("##########")
+print("#  {}  {}  #".format(eye,eye))
+print("#  ----  #")
+print("##########")
---
+++ basics/input/user_input.py
@@ -0,0 +1,19 @@
+# Ask user to enter their name
+print("What is your name, human?")
+name = input()
+
+
+print("How old in years?")
+age = input()
+
+print("How tall are you in metres?")
+height = int(input())
+
+print("Weight in kg?")
+weight = int(input())
+
+#calculate bmi
+bmi = weight/(height**2)
+
+#print with 2dp format
+print("{0} you are {1} years old and your bmi is {2:.2f}".format(name, age,bmi))
