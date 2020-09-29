68d25c1d6fc2fc1c2ef78b2a41e71e3ad3cd377f
repl.it user <replituser@example.com>
2020-09-29 10:19:41 +0000

    created basic input output program

---
+++ basics/input/review.py
@@ -0,0 +1,9 @@
+print("give me a name")
+name = input()
+
+print("number now")
+num = int(input())
+
+print(f"Your number is: \n \t {num}")
+print(f"Your name: \n \t {name}")
+print(f"Printing {name} {num} times: \n \t {name*num}")
---
+++ basics/input/string_operators.py
@@ -0,0 +1,14 @@
+print("Please enter the number of lives.")
+lives = int(input())
+
+print("Please enter the energy level.")
+nrg = int(input())
+
+print("Please enter the shield level.")
+shields = int(input())
+
+print("Health has been set.")
+
+print("Lives: {}".format("<3 "*lives))
+print("Energy: {}".format("*"*nrg))
+print("Shield: {}".format("*"*shields))
