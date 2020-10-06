854b0ce0972aa92b974a0805fa6eefd41294aaa9
wtumber <tumberw@gmail.com>
2020-10-06 10:13:48 +0100

    Added code to demonstrate use of an if elif else

---
+++ basics/decisions/simple_decision/if_elif_else.py
@@ -0,0 +1,11 @@
+print("up, down,left, or right?")
+direction = input()
+
+if direction == "up":
+    print("up")
+elif direction == "down":
+    print("down")
+elif direction == "left":
+    print("left")
+else:
+    print("right")
