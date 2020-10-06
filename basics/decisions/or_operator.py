28d2961aed5b071d5fdf07f0a6fe564d3c6ef478
wtumber <tumberw@gmail.com>
2020-10-06 10:48:20 +0100

    Added code to demonstrate the use of or operator

---
+++ basics/decisions/or_operator.py
@@ -0,0 +1,9 @@
+print("What type of adventure:Scary, short, safe, long")
+advent_type = input()
+
+if advent_type == "scary" or advent_type == "short":
+    print("Dark forest then")
+elif advent_type == "long" or advent_type == "safe":
+    print("Safe route then")
+else:
+    print("wrong entry")
