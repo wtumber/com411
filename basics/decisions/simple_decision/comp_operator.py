36807d27b0c962ac21a959dbd7b806963dd8d58f
wtumber <tumberw@gmail.com>
2020-10-06 10:28:02 +0100

    Add file using compariosn operators

---
+++ basics/decisions/simple_decision/comp_operator.py
@@ -0,0 +1,11 @@
+print("first num:")
+num1 = int(input())
+print("num 2:")
+num2 = int(input())
+
+if num1 < num2 :
+    print("num1 is smallest")
+elif num2 == num1 :
+    print("same number")
+else:
+    print("num2 is smallest")
