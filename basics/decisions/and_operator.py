b18fded8509a84993ba06c179baae5e9c56eb53a
wtumber <tumberw@gmail.com>
2020-10-06 10:50:27 +0100

    Added code to demonstrate the use of logical and operator

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,10 @@
+print("What was that?")
+hear = input()
+
+print("what did I see?")
+see = input()
+
+if hear == "grr" and see == "red eyes":
+    print("I'm leaving")
+else:
+    print("continuing")
