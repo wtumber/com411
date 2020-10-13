c3e3c2969340f9e7ae3d540c50cfbb12669a5eb9
wtumber <tumberw@gmail.com>
2020-10-13 10:41:59 +0100

    Added code to demonstrate the use of a user defined function with a parameter

---
+++ basics/functions/function_with_parameter.py
@@ -0,0 +1,13 @@
+def escape_by(plan):
+    if plan == "jumping over":
+        print("Cant")
+    elif plan == "running around":
+        print("cant")
+    else:
+        print("okay that might work")
+
+escape_by("jumping over")
+
+escape_by("running around")
+
+escape_by("going deeper")    
