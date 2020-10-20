aaa83cc7d65c1afe3eed5623f91ded798b6238ab
wtumber <tumberw@gmail.com>
2020-10-20 09:58:05 +0100

    Added code to demonstrate simple list

---
+++ data/lists/simple_list.py
@@ -0,0 +1,7 @@
+def directions():
+    directions = []
+    directions.append("Move Forward")
+    directions.append("Move Backward")
+    directions.append("Turn Left")
+    directions.append("Turn Right")
+    return directions
