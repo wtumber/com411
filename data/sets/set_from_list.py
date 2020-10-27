0db137c00d84a3471a5e7e9bdca4aa4e2131a2e3
wtumber <tumberw@gmail.com>
2020-10-27 09:21:37 +0000

    Added code to demonstrate how to create a set from a list

---
+++ data/sets/set_from_list.py
@@ -0,0 +1,19 @@
+def observed():
+    observations = []
+    while len(observations)<7:
+        print("Input a place:")
+        observations.append(input())
+    return observations
+
+def run():
+    print("Counting observations...")
+    obs = observed()
+    obs_count = set()
+    for place in obs:
+        place_count = obs.count(place)
+        obs_count.add((place,place_count))
+    print(obs_count)
+    for tup in obs_count:
+        print("{} observed {} times".format(tup[0],tup[1]))
+
+run()
