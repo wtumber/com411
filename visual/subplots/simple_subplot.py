7f2311ac19a1b5f1cc482e8d65662887b4e15d1a
wtumber <tumberw@gmail.com>
2020-11-17 09:47:51 +0000

    Added code to display simple subplots

---
+++ visual/subplots/simple_subplot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+def read_data(file_path):
+    with open(file_path) as file:
+        file_info = []
+        for line in file:
+            file_info.append(line)
+        file.close()
+    return file_info
+
+def run():
+    data = read_data("C:/Users/ISA06006086/Documents/University/com411/visual/subplots/temps.txt")
+    fig, axes = plt.subplots(1,2)
+    axes[0].plot(data)
+    axes[1].bar(data,data)
+    plt.show()
+
+run()
+
+
+
